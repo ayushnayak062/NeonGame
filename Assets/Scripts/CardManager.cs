1ff61f9873f294824543a9b819761eff3eaf17e1
Ayush Nayak <ayushnayak062@gmail.com>
2025-09-04 15:45:58 +0530

    Add difficulty layouts and random card selection
    Implemented difficulty levels:
    - Easy
    - Medium
    - Hard
    -Insane
    
    Added runtime difficulty selection using UI buttons.
    Card spawning now selects random pairs from available faces.
    Each playthrough uses different card sets for better replay ability.

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -2,47 +2,109 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Media;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum Difficulty { Easy, Medium, Hard , Insane}
 
 public class CardManager : MonoBehaviour
 {
     public static CardManager instance;
 
     public Card cardPrefab;
-
     public Sprite cardBack;
     public Sprite[] cardFaces;
+    public Transform cardHolder;
+
+    public Difficulty currentDifficulty = Difficulty.Easy;
+
     private List<Card> cards;
     private List<int> cardValues;
     public Card firstCard, secondCard;
-    public Transform cardHolder;
+
+    private int rows, cols;
 
     private void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else if (instance != this)
-        {
-            Destroy(gameObject);
-        }
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
     }
 
     void Start()
     {
-        cards = new List<Card>();
-        cardValues = new List<int>();
+        SetupLayout();
         CreateCards();
         ShuffleCards();
+        Debug.Log(currentDifficulty);
     }
+
+    void SetupLayout()
+    {
+        switch (currentDifficulty)
+        {
+            case Difficulty.Easy:
+                rows = 3; cols = 2;
+                break;
+            case Difficulty.Medium:
+                rows = 4; cols = 3;
+                break;
+            case Difficulty.Hard:
+                rows = 5; cols = 4;
+                break;
+            case Difficulty.Insane:
+                rows = 5; cols = 6;
+                break;
+        }
+
+        // Adjust GridLayoutGroup
+        GridLayoutGroup grid = cardHolder.GetComponent<GridLayoutGroup>();
+        if (grid != null)
+        {
+            grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            grid.constraintCount = cols;
+
+            // auto-size cell depending on holder
+            float cellWidth = (cardHolder.GetComponent<RectTransform>().rect.width - grid.spacing.x * (cols - 1)) / cols;
+            float cellHeight = (cardHolder.GetComponent<RectTransform>().rect.height - grid.spacing.y * (rows - 1)) / rows;
+            /*grid.cellSize = new Vector2(cellWidth, cellHeight);*/
+        }
+    }
+    public void SetDifficulty(int difficultyIndex)
+    {
+        currentDifficulty = (Difficulty)difficultyIndex;
+        RestartGame();
+    }
+
 
     void CreateCards()
     {
-        for (int i = 0; i < cardFaces.Length / 2; i++)
+        cards = new List<Card>();
+        cardValues = new List<int>();
+
+        int totalCards = rows * cols;
+        int pairs = totalCards / 2;
+
+        // pick random unique card IDs
+        List<int> availableFaces = new List<int>();
+        for (int i = 0; i < cardFaces.Length; i++)
+            availableFaces.Add(i);
+
+        // shuffle available faces
+        for (int i = 0; i < availableFaces.Count; i++)
         {
-            cardValues.Add(i);
-            cardValues.Add(i);
+            int randomIndex = Random.Range(i, availableFaces.Count);
+            int temp = availableFaces[i];
+            availableFaces[i] = availableFaces[randomIndex];
+            availableFaces[randomIndex] = temp;
         }
+
+        // take only the required number of pairs
+        for (int i = 0; i < pairs; i++)
+        {
+            cardValues.Add(availableFaces[i]);
+            cardValues.Add(availableFaces[i]);
+        }
+
+        // now instantiate cards
         foreach (int id in cardValues)
         {
             Card newCard = Instantiate(cardPrefab, cardHolder);
@@ -51,6 +113,8 @@ public class CardManager : MonoBehaviour
             cards.Add(newCard);
         }
     }
+
+
 
     void ShuffleCards()
     {
@@ -102,6 +166,22 @@ public class CardManager : MonoBehaviour
         secondCard.HideCard();
         firstCard = null;
         secondCard = null;
+    }
+
+    void RestartGame()
+    {
+        // Destroy old cards
+        foreach (Transform child in cardHolder)
+            Destroy(child.gameObject);
+
+        // Reset state
+        firstCard = null;
+        secondCard = null;
+
+        // Re-setup
+        SetupLayout();
+        CreateCards();
+        ShuffleCards();
     }
 }
 
